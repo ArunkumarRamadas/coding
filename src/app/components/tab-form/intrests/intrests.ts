3d0d2646b989ccea515fdf99870b400a9bf1a26c
ArunkumarRamadas <aramadas@logitech.com>
2026-04-22 16:33:54 +0530

    Initial commit

---
+++ src/app/components/tab-form/intrests/intrests.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-intrests',
+  imports: [],
+  templateUrl: './intrests.html',
+  styleUrl: './intrests.scss',
+})
+export class Intrests {
+
+}
---
+++ src/app/components/tab-form/tab-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-form',
+  imports: [],
+  templateUrl: './tab-form.html',
+  styleUrl: './tab-form.scss',
+})
+export class TabForm {
+
+}
